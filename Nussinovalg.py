a211d511433851e8f8f8064f388a7d06c4e57fbf
Nada Elgendy <76693809+nadaelgendy2@users.noreply.github.com>
2022-04-25 23:17:37 +0200

    Update Nussinovalg.py

--- Nussinovalg.py
+++ Nussinovalg.py
@@ -1,15 +1,44 @@
 import numpy as np
 
-dict={('G,C') : 1,('A,U'):1,('G,U'):1}
-#print(dict[('G,C')])
+Pairsdict={  'G,C' : 1,  'A,U':1,  'G,U':1, 'C,G' : 1   ,'U,A':1,  'U,G':1}
 
-def ZeroMatrix(row, column):
+def ZeroMatrix(seq):
+
     #print( [[0 for row in range(row)] for column in range(column)])
-    matrix=np.zeros((row,column),int)
-    np.fill_diagonal(matrix,1)
-    print(matrix)
+    matrix=np.zeros((len(seq),len(seq)),int)
+    np.fill_diagonal(matrix,0)
+   # print(matrix)
     return matrix
 
-ZeroMatrix(5,5)
+
+def Check(seq):
+    if seq in Pairsdict:
+        return Pairsdict[seq]
+    else:
+        return -1
+
+
+def MatrixFilling(matrix,RnaSeq):
+    for index in range(1, len(RnaSeq)):
+        for row in range(len(RnaSeq) - index):
+            column = row + index
+            if column - row >= 0:
+                Leftindex = matrix[row][column - 1]
+                belowindex = matrix[row + 1][column]
+                seq=RnaSeq[row]+','+ RnaSeq[column]
+                Diagnoalindex = matrix[row + 1][column - 1] +Check(seq)
+               # rc = max([matrix[row][t] + matrix[t + 1][column] for t in range(row, column)])  # 4th rule #pifurcation
+                matrix[row][column] = max(Leftindex,belowindex,Diagnoalindex)  # max of all
+            else:
+                matrix[row][column] = -1
+    return matrix
+
+
+#"GGGAAAUCC"
+rnaseq=input("Enter RNA Sequence : ").upper()
+matrix=ZeroMatrix(rnaseq)
+matrix=MatrixFilling(matrix,rnaseq)
+print(matrix)
+
 
 
