c2d007c2076b531b746a4428333c2b57fd69c260
Menna Hassanin <88107705+mennaHassanin@users.noreply.github.com>
2022-04-30 00:39:45 +0200

    Update Nussinovalg.py

--- Nussinovalg.py
+++ Nussinovalg.py
@@ -1,7 +1,7 @@
 import numpy
 import numpy as np
 #MA7DSH YL3AB F AY HAGA 3ASHAN HAYDRBKO ALLAHUMA BALGHT!
-Pairsdict={  'G,C' : 1,  'A,U':1,  'G,U':1, 'C,G' : 1   ,'U,A':1,  'U,G':1}
+Pairsdict={  'G,C' : 1,  'A,U':1, 'C,G' : 1,'U,A':1,'G,U':1, 'U,G':1}
 
 def ZeroMatrix(seq):
 
@@ -19,50 +19,94 @@ def Check(seq):
         return -1
 
 
-def MatrixFilling(matrix,RnaSeq):
+def MatrixFilling(matrix, RnaSeq):
+    DirectionList ={}
+
     for index in range(1, len(RnaSeq)):
         for row in range(len(RnaSeq) - index):
             column = row + index
+
             if column - row >= 0:
                 Leftindex = matrix[row][column - 1]
                 belowindex = matrix[row + 1][column]
                 seq=RnaSeq[row]+','+ RnaSeq[column]
                 Diagnoalindex = matrix[row + 1][column - 1] +Check(seq)
+
                 bir=[]
                 birmax=0
+
                 for t in range(row,column):
                   bir.append(matrix[row][t]+matrix[t + 1][column])
+
                 for t in range(row,column):
                     birmax=max(bir)
 
-                matrix[row][column] = max(Leftindex,belowindex,Diagnoalindex,birmax)  # max of all
+                matrix[row][column] = max(Leftindex,belowindex,Diagnoalindex,birmax)
+                if Leftindex == matrix[row][column]:
+                    DirectionList[str(row),str(column)]='Left'
 
-                #saving the indices of the max each time
-                leftind={}
-                belowind = {}
-                diagind = {}
-                birind = {}
-                if matrix[row][column] == Leftindex:
-                    leftind={row,column}
+                elif belowindex == matrix[row][column]:
+                    DirectionList[str(row),str(column)] = 'Below'
 
-                elif matrix[row][column] == belowindex:
-                    belowind = {row, column}
-
-                elif matrix[row][column] == Diagnoalindex:
-                    diagind = {row,column}
+                elif Diagnoalindex == matrix[row][column]:
+                    DirectionList[str(row),str(column)] = 'Diagonal'
 
                 else:
-                    for k in range(row + 1, column - 1):
-
-                        if matrix[row][column] == birmax:
-                            birind = {row,column}
-
-                #print(birind,diagind,belowind,leftind)
-
+                    DirectionList[str(row),str(column)] = 'Burification'
             else:
                 matrix[row][column] = -1
+
+    tracebackList=[]
+    outputDots = []
+
+    tracebackList=traceback(RnaSeq, matrix, tracebackList, 0, len(RnaSeq)-1,DirectionList)
+
+    for element in range(len(RnaSeq)):
+        outputDots.append('.')
+    for element in tracebackList:
+        outputDots[min(element)] = "("
+        outputDots[max(element)] = ")"
+
+    Output="".join(outputDots)
+    print(Output)
+
     return matrix
 
+def traceback(RnaSeq, matrix, TracebackList, i, j, DirectionList):
+
+    if i < j:
+        if DirectionList[str(i),str(j)] == 'Below':
+
+            traceback(RnaSeq, matrix, TracebackList, i + 1, j, DirectionList)
+
+        elif DirectionList[str(i),str(j)] == 'Left':
+
+            traceback(RnaSeq, matrix, TracebackList, i, j - 1, DirectionList)
+
+        elif DirectionList[str(i),str(j)] == 'Diagonal':
+
+            TracebackList.append((i, j))
+
+            traceback(RnaSeq, matrix, TracebackList, i + 1, j - 1, DirectionList)
+
+        elif DirectionList[str(i),str(j)] == 'Burification':
+            maximumK=i+1
+
+            while maximumK < j:
+
+                if matrix[i][j] == matrix[i][maximumK]+matrix[maximumK+1][j]:
+
+                    traceback(RnaSeq, matrix, TracebackList, i, maximumK, DirectionList)
+
+                    traceback(RnaSeq,matrix, TracebackList, maximumK + 1, j,DirectionList)
+
+                    break
+                maximumK=maximumK+1
+
+    elif i > j:
+        return TracebackList
+
+    return TracebackList
 
 #"GGGAAAUCC"
 #"CGGACCCAGACUUUC"
@@ -71,4 +115,3 @@ rnaseq=input("Enter RNA Sequence : ").upper()
 matrix=ZeroMatrix(rnaseq)
 matrix=MatrixFilling(matrix,rnaseq)
 print(matrix)
-
